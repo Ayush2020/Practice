66ec109bc0c335499e522f27e7ace85eee44c5af
Ayush <ayushgiri2021itc@gmail.com>
2026-01-08 14:14:00 +0530

    sets 1-2

---
+++ python/set.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+set = {1,2,3,4,5,6,7, (1,2,3), "Hello"}
+# set.pop()
+
+
+# set.remove(4)
+# set.remove(10) gives error if wont finds the value
+
+# set.discard("Hello")
+# set.discard(25) dont gives the error
+
+
+# set.clear()
+
+# del set  delte the set
+print(set)
+
+
---
+++ python/set2.py
@@ -0,0 +1,28 @@
+# Intersection 
+
+s1 = {1,2,3,4,5}
+s2 = {2,7,8,9}
+
+# res = s1.intersection(s2)
+# print(res)
+
+
+#Union
+# rs = s1.union(s2)
+# print(rs)
+
+
+#difference 
+# res = s1.difference(s2)
+# print(re)
+
+
+#diffrerence update
+# s1.difference_update(s2)
+# print(s1)
+
+
+#boolean
+
+res = s1.isdisjoint(s2)
+print(res)
