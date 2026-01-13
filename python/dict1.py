ed24c5434545f8f225e13ce231277755464c96ea
Ayush <ayushgiri2021itc@gmail.com>
2026-01-13 19:23:52 +0530

    Dictionary

---
+++ python/dict1.py
@@ -0,0 +1,25 @@
+# dict = {1:100, 2:200, 3:300}
+# print(dict)
+
+
+
+# dict.keys() used when we need keys only
+# dict.values() used when we need values only 
+# dict.items() when we need both keys nd values both but it wont returns in dict format
+
+# a = dict[1]
+
+
+dict = {"hello":[1,3,4,5], "hi":[1,2], 1:34, 4:"helo"}
+# a = dict["hello"][-1]
+# dict["hiii"] = 100
+
+
+dict.update([[6,36], (7, [1,2,3])])
+print(dict)
+
+
+
+
+
+
