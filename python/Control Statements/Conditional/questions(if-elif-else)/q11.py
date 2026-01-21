956f1ec04b69d50257d64badb8ec20767678a000
Ayush <ayushgiri2021itc@gmail.com>
2026-01-21 20:31:16 +0530

    Added 11 th Question

---
+++ python/Control Statements/Conditional/questions(if-elif-else)/q11.py
@@ -0,0 +1,6 @@
+#Wap to check whether the last character of given string is special character or not
+str = input("Enter a string: ")
+if not str[-1].isalnum():
+    print("special character")
+else:
+    print("special character")
