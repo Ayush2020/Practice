e83159e96f8fedd508c5029178a43168b4c0d5ca
Ayush <ayushgiri2021itc@gmail.com>
2026-02-02 14:25:06 +0530

    Addedd

--- python/Operators/bitwise.py
+++ python/Operators/bitwise.py
@@ -2,3 +2,42 @@
 # .............Bitwise operators  (&)
  
 
+# x = 0
+# if x and (10/x+1):
+#   print("hello")
+# else:
+#   print("hyee")
+
+
+if 13 & 14:
+  print("hii")
+else:
+  print("hello")
+
+
+if 13 | 14:
+  print("hii")
+else:
+  print("hello")
+
+if 13 >> 14:
+  print("hii")
+else:
+  print("hello")
+
+
+if 13 << 14:
+  print("hii")
+else:
+  print("hello")
+
+
+if 13 ^ 14:
+  print("hii")
+else:
+  print("hello")
+
+if ~13:
+  print("hii")
+else:
+  print("hello")
