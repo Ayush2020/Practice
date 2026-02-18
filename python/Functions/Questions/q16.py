63226060a950bf215b31721bafb0808baeafb41a
Ayush <ayushgiri2021itc@gmail.com>
2026-02-19 01:05:47 +0530

    Made Changes

--- python/Functions/Questions/q16.py
+++ python/Functions/Questions/q16.py
@@ -1,9 +1,13 @@
 #WAP to print all the names from a list and their first character should be capital and rest of them should be like that only without built in function only
 ls = ['vaidehi', 'rahul', 'shivam']
+newls = []
 
-for i in ls:
-  if 'a' <= i[0] <= 'z':
-    print(chr(ord(i[0]) - 32) + i[1::])
+
+def capital(ls):
+  for i in ls:
+    if 'a' <= i[0] <= 'z':
+      a = chr(ord(i[0]) - 32) + i[1::]
+      newls.append(a)
+  return newls
   
-
-
+print(capital(ls))
